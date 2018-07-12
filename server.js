efb6d619257ef19cb61e87c23fc16768f7dbb0fe
332938647@qq.com <332938647@qq.com>
2018-07-12 09:39:28 +0800

    clean file

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
-// var cors = require('cors')
-// var path = require('path')
+
 
 http.listen(4000, function () {
   console.log('listening on *:4000')
